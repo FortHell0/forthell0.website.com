7bb03187d30f5c08dda66f110481a98d2bf94d03
FortHell0 <forthellvok@gmail.com>
2023-01-26 20:34:22 +0100

    Made stuff

---
+++ FirstJS.js
@@ -0,0 +1,8 @@
+console.log('HTML JS CONSOLE TEST');
+
+const btn = document.querySelector('.buttonClass1');
+
+btn.onclick = () => {
+    alert('Hello World!');
+    console.log('Button Press');
+}
